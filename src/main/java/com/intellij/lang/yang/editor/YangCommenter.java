c6337472996da1d46d8d30369a6ca48646b7a46a
jobo <exjobo@gmail.com>
2018-07-26 12:58:28 +0900

    add commenter

---
+++ src/main/java/com/intellij/lang/yang/editor/YangCommenter.java
@@ -0,0 +1,39 @@
+package com.intellij.lang.yang.editor;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * YangCommenter
+ */
+public class YangCommenter implements Commenter {
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return "//";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return "/*";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return "*/";
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
